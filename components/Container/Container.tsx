5bd19b7c9d4d97516d856ca29e497b3ad4cdbecb
ott <ott@debian>
2023-08-08 13:49:06 -0300

    v1.1

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -1,10 +1,12 @@
-import React from "react";
-
-export default function Container(props){
+import {ReactNode} from "react";
+type Props = {
+    children: ReactNode
+}
+export default function Container({children}:Props){
     return(
         <div className={`w-full h-full mx-auto px-5
         max-w-[600px] sm:max-w-[900px] md:max-w-[1000px]  lg:max-w-[1000px] xl:max-w-[1240px] 3xl:max-w-[1400px] mt-[50px]`}>
-            {props.children}
+            {children}
         </div>
     )
 
