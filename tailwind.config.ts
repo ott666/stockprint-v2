a1a55ce7601d58b73fdfaa2d425b8461ccbff990
ott666 <ottaviocavarzani@gmail.com>
2023-08-13 20:38:01 -0300

    v1.4.2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,8 @@ const config: Config = {
           'url("/logos/hid_gold.svg")',
           'ink1000card':
           'url("/ink1000/ink10007.png")',
-          
+          'gradienteAzul':'linear-gradient(135deg, rgba(106, 121, 172, 0.37) 100%, rgba(18, 65, 234, 0.37) 100%);',
+          'gradienteAzulRisk':'linear-gradient(135deg, rgba(20, 0, 255, 1) 21.88%, rgba(59, 111, 186, 1) 55.21%, rgba(0, 56, 255, 1) 84.90%)',
       },
     },
   },
